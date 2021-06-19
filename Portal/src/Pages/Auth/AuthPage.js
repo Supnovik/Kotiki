f173914279dbace4df302df2b3b8f99316727ae2
s-SoMka-s <i.chiesov@gmail.com>
2021-06-20 00:31:09 +0700

    Add login page.

--- Portal/src/Pages/Auth/AuthPage.js
+++ Portal/src/Pages/Auth/AuthPage.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './AuthPage.scss'
 
 function AuthPage() {
     return (
         <div className="auth-page">
-            <div className="auth-page__title">Welcome</div>
-            <input className="auth-page__input" placeholder="Email Address"></input>
-            <input className="auth-page__input" placeholder="*******" type="password"></input>
-            <button className="auth-page__button">Login</button>
+            <div className="auth-page__form">
+                <div className="auth-page__title">Welcome</div>
+                <input
+                    className="auth-page__input"
+                    placeholder="Email Address"
+                ></input>
+                <input
+                    className="auth-page__input"
+                    placeholder="*******"
+                    type="password"
+                ></input>
+                <Link to="/pages/dashboard" className="auth-page__button">
+                    Login
+                </Link>
+            </div>
         </div>
     )
 }
